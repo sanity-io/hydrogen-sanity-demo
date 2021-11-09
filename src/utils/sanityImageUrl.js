1ae54cfdec2ba85618b9d3123813ada4d80bbde0
Robin Pyon <robin.pyon@gmail.com>
2021-11-09 11:15:42 +0000

    refactor: don't pass client to @sanity/image-url

--- src/utils/sanityImageUrl.js
+++ src/utils/sanityImageUrl.js
@@ -1,10 +1,12 @@
-import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 import sanityConfig from '../../sanity.config';
 
 const sanityImageUrl = (image, {width} = {width: 800}) => {
-  const builder = imageUrlBuilder(sanityClient(sanityConfig));
+  const builder = imageUrlBuilder({
+    dataset: sanityConfig.dataset,
+    projectId: sanityConfig.projectId,
+  });
   const urlFor = builder.image(image);
 
   const src = urlFor.width(width).auto('format').quality(80).url();
