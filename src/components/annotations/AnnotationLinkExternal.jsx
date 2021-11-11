63f4a0d2a5b2542e6218a35902e3be9557a76952
Robin Pyon <robin.pyon@gmail.com>
2021-11-11 17:51:10 +0000

    style: re-instate underlines in all external links

--- src/components/annotations/AnnotationLinkExternal.jsx
+++ src/components/annotations/AnnotationLinkExternal.jsx
@@ -3,7 +3,7 @@ const AnnotationLinkExternal = (props) => {
 
   return (
     <a
-      className="duration-300 inline-flex font-medium hover:opacity-60 items-center text-black transition-opacity"
+      className="duration-300 inline-flex font-medium hover:opacity-60 items-center text-black transition-opacity underline"
       href={mark?.url}
       rel="noopener noreferrer"
       target={mark?.newWindow ? '_blank' : '_self'}
