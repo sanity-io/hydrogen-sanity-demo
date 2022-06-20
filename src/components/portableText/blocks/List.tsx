d984e73b7f09d3473c50c82bf7e4fe3328fe4f78
Robin Pyon <robin.pyon@gmail.com>
2022-06-20 14:11:57 +0100

    fix: correctly render nested and unordered lists

--- src/components/portableText/blocks/List.tsx
+++ src/components/portableText/blocks/List.tsx
@@ -2,36 +2,18 @@ import clsx from 'clsx';
 
 const SHARED_LIST_CLASSES = clsx(
   'first:mt-0 last:mb-0', //
-  'my-8 space-y-0.5 leading-paragraph list-inside',
+  'my-8 space-y-0.5 leading-paragraph list-outside ml-8',
 );
 
 export default function ListBlock(props: any) {
   const {children, type} = props;
 
   if (type === 'bullet') {
-    return (
-      <ol
-        className={clsx(
-          'list-disc', //
-          SHARED_LIST_CLASSES,
-        )}
-      >
-        {children}
-      </ol>
-    );
+    return <ul className={SHARED_LIST_CLASSES}>{children}</ul>;
   }
 
   if (type === 'number') {
-    return (
-      <ol
-        className={clsx(
-          'list-decimal', //
-          SHARED_LIST_CLASSES,
-        )}
-      >
-        {children}
-      </ol>
-    );
+    return <ol className={SHARED_LIST_CLASSES}>{children}</ol>;
   }
 
   return null;
