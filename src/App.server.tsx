8d58cfeb944e9004ee1246e89b9a982fabba6b92
Robin Pyon <robin.pyon@gmail.com>
2022-06-27 14:55:01 +0100

    feat: hydrogen 1.0.2

--- src/App.server.tsx
+++ src/App.server.tsx
@@ -1,6 +1,5 @@
 import {
   FileRoutes,
-  LocalizationProvider,
   PerformanceMetrics,
   PerformanceMetricsDebug,
   Route,
@@ -10,6 +9,7 @@ import {
   useSession,
 } from '@shopify/hydrogen';
 import renderHydrogen from '@shopify/hydrogen/entry-server';
+import type {CountryCode} from '@shopify/hydrogen/storefront-api-types';
 import {Suspense} from 'react';
 import ServerCartProvider from './components/cart/ServerCartProvider.server';
 import DefaultSeo from './components/DefaultSeo.server';
@@ -21,20 +21,18 @@ function App() {
 
   return (
     <Suspense fallback={<LoadingFallback />}>
-      <ShopifyProvider>
-        <LocalizationProvider countryCode={session?.countryCode}>
-          <ServerCartProvider>
-            <DefaultSeo />
-            <Router>
-              <FileRoutes />
-              {/* @ts-expect-error <NotFound> doesn't require response */}
-              <Route path="*" page={<NotFound />} />
-            </Router>
-          </ServerCartProvider>
-          <PerformanceMetrics />
-          {import.meta.env.DEV && <PerformanceMetricsDebug />}
-          <ShopifyAnalytics />
-        </LocalizationProvider>
+      <ShopifyProvider countryCode={session?.countryCode as CountryCode}>
+        <ServerCartProvider>
+          <DefaultSeo />
+          <Router>
+            <FileRoutes />
+            {/* @ts-expect-error <NotFound> doesn't require response */}
+            <Route path="*" page={<NotFound />} />
+          </Router>
+        </ServerCartProvider>
+        <PerformanceMetrics />
+        {import.meta.env.DEV && <PerformanceMetricsDebug />}
+        <ShopifyAnalytics />
       </ShopifyProvider>
     </Suspense>
   );
